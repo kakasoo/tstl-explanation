a8af1c68d5e1008f44f14560e7ab80c2bb7cbb6e
kakasoo <kscodebase@gmail.com>
2023-04-08 17:19:44 +0900

    13. IPush, ISize 함수부 정의

---
+++ src/interface/partial/IPush.ts
@@ -0,0 +1,12 @@
+/**
+ * src/internal/container/partial/IPush.ts
+ */
+export interface IPush<T> {
+    /**
+     * Insert items at the end.
+     *
+     * @param items Items to insert.
+     * @return Number of elements in the container after insertion.
+     */
+    push(...items: T[]): number;
+}
---
+++ src/interface/partial/ISize.ts
@@ -0,0 +1,9 @@
+/**
+ * src/internal/container/parital/ISize.ts
+ */
+export interface ISize {
+    /**
+     * Number of elements in the container.
+     */
+    size(): number;
+}
