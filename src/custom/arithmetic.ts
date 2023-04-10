4a7d6465d8e0c986eb6851bfb6e1c95b691e8c22
kakasoo <kscodebase@gmail.com>
2023-04-10 22:50:42 +0900

    24. 10의 몇승 이상인지를 의미하는 NDigit 타입 선언

--- src/custom/arithmetic.ts
+++ src/custom/arithmetic.ts
@@ -1,3 +1,5 @@
+import { Length, Split } from "./utility";
+
 export interface StringToNumberMap {
     "0": 0;
     "1": 1;
@@ -9,6 +11,13 @@ export interface StringToNumberMap {
     "8": 8;
     "9": 9;
 }
+
+/**
+ * 자리수를 의미하며, T가 1의 자리면 1, 10의 자리면 2, 100의 자리면 3, 이런 식으로 10의 N승 이상, N + 1 승 미만을 의미
+ */
+export type NDigit<T extends string> = T extends `${number}`
+    ? Length<Split<T>>
+    : never;
 
 export type StringToNumberTuple<T extends string> =
     T extends `${infer F}${infer Rest}`
