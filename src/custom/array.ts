7a1a2ca5e8a31c5d9f713aa4dba3f75dd359e278
kakasoo <kscodebase@gmail.com>
2023-04-11 00:50:23 +0900

    26. 나머지 연산들도 추가
    추후에는 배열 요소 중 1개라도 never가 있으면 결과를 never로 반환하개끔 수정해야 한다
    또는, 마이너스 연산이 가능하게 수정해야 한다

--- src/custom/array.ts
+++ src/custom/array.ts
@@ -1,4 +1,4 @@
-import { Add, Multiply } from "./math";
+import { Add, Divide, Multiply, Remainder, Sub } from "./math";
 
 export type AddMap<T extends readonly number[], N extends number> = {
     [K in keyof T]: Add<T[K], N>;
@@ -10,8 +10,16 @@ export type MultiplyMap<T extends readonly number[], N extends number> = {
 
 export type Map<
     T extends readonly number[],
-    OP extends "Add" | "Multiply",
+    OP extends "Add" | "Multiply" | "Sub" | "Divide" | "Remainder",
     N extends number
 > = OP extends "Add"
     ? { [K in keyof T]: Add<T[K], N> }
-    : { [K in keyof T]: Multiply<T[K], N> };
+    : OP extends "Multiply"
+    ? { [K in keyof T]: Multiply<T[K], N> }
+    : OP extends "Sub"
+    ? { [K in keyof T]: Sub<T[K], N> }
+    : OP extends "Divide"
+    ? { [K in keyof T]: Divide<T[K], N> }
+    : OP extends "Remainder"
+    ? { [K in keyof T]: Remainder<T[K], N> }
+    : never;
