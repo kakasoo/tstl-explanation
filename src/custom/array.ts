d9e293696e74dfecc9fda658f571aacdc1371791
kakasoo <kscodebase@gmail.com>
2023-04-11 00:55:41 +0900

    27. Map type에 대한 설명 추가하고, 중간 경유하려고 만든 AddMap, MultiplyMap 삭제

--- src/custom/array.ts
+++ src/custom/array.ts
@@ -1,13 +1,14 @@
 import { Add, Divide, Multiply, Remainder, Sub } from "./math";
 
-export type AddMap<T extends readonly number[], N extends number> = {
-    [K in keyof T]: Add<T[K], N>;
-};
-
-export type MultiplyMap<T extends readonly number[], N extends number> = {
-    [K in keyof T]: Multiply<T[K], N>;
-};
-
+/**
+ * 사칙연산을 튜플에 적용한 타입
+ *
+ * type a = Map<[3, 4, 5], "Add", 2>; // [5, 6, 7]
+ * type b = Map<[3, 4, 5], "Multiply", 2>; // [6, 8, 10]
+ * type c = Map<[3, 4, 5], "Sub", 2>; // [1, 2, 3]
+ * type d = Map<[3, 4, 5], "Divide", 2>; // [1, 2, 2]
+ * type e = Map<[3, 4, 5], "Remainder", 2>; // [1, 0, 1]
+ */
 export type Map<
     T extends readonly number[],
     OP extends "Add" | "Multiply" | "Sub" | "Divide" | "Remainder",
