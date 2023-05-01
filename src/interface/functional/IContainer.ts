26cb1abd6f39ebe908a45c297cb964c579dfc32c
kakasoo <kscodebase@gmail.com>
2023-05-01 14:43:24 +0900

    43. 제너릭 역 추론을 위해 Pelem 타입 파라미터 추가

--- src/interface/functional/IContainer.ts
+++ src/interface/functional/IContainer.ts
@@ -3,20 +3,29 @@ import { IReversableIterator } from "../iterator/IReversableIterator";
 import { IBidirectionalContainer } from "./IBidirectionalContainer";
 import { IEmpty } from "../partial/IEmpty";
 import { ISize } from "../partial/ISize";
+import { IPush } from "../partial/IPush";
 /**
  * src/base/container/IContainer.ts
  */
 export interface IContainer<
-    T,
+    T extends PElem,
     IteratorT extends IContainer.Iterator<T, IteratorT, ReverseT>,
-    ReverseT extends IContainer.ReverseIterator<T, IteratorT, ReverseT>
+    ReverseT extends IContainer.ReverseIterator<T, IteratorT, ReverseT>,
+    /**
+     * 제너릭 역 추론
+     *
+     * 앞으로 구현될 자료구조들은 IContainer를 상속하여 만들어진다.
+     * 이 자료구조를 A라고 할 때, 그 A의 데이터 타입 T를 추론하기 위해 만들어진다.
+     */
+    PElem = T
 > extends IBidirectionalContainer<
             IContainer.Iterator<T, IteratorT, ReverseT>,
             ReverseT
         >,
         Iterable<T>,
         IEmpty,
-        ISize {}
+        ISize,
+        IPush<PElem> {}
 
 /**
  * IContainer가 가지는 내부 속성
