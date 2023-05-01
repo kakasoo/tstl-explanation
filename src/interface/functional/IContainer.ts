79b536b8aa4f2ddf283f7296e19825a88087a43d
kakasoo <kscodebase@gmail.com>
2023-05-01 17:53:03 +0900

    45. SourceT, PElem 타입 파라미터 정의
    - 현재 타입의 더 구체화된 쪽의 타입 ( 여기를 부모 타입이라 명명 ) 을 가리키는 타입들이다.
    - SoucreT는 자기 자신을 가리키게 하기 위한 용도로 작성되었으며,
    - PElem은 부모의 타입을 추론하기 위해 만들어졌다.

--- src/interface/functional/IContainer.ts
+++ src/interface/functional/IContainer.ts
@@ -4,13 +4,26 @@ import { IBidirectionalContainer } from "./IBidirectionalContainer";
 import { IEmpty } from "../partial/IEmpty";
 import { ISize } from "../partial/ISize";
 import { IPush } from "../partial/IPush";
+
 /**
  * src/base/container/IContainer.ts
+ *
+ * @template T Stored elements' type
+ * @template SourceT Derived type extending this {@link IContainer}
+ * @template IteratorT Iterator type
+ * @template ReverseT Reverse iterator type
+ * @template PElem Parent type of *T*, used for inserting elements through {@link assign} and {@link insert}.
  */
 export interface IContainer<
     T extends PElem,
-    IteratorT extends IContainer.Iterator<T, IteratorT, ReverseT>,
-    ReverseT extends IContainer.ReverseIterator<T, IteratorT, ReverseT>,
+    SourceT extends IContainer<T, SourceT, IteratorT, ReverseT, PElem>,
+    IteratorT extends IContainer.Iterator<T, SourceT, IteratorT, ReverseT>,
+    ReverseT extends IContainer.ReverseIterator<
+        T,
+        SourceT,
+        IteratorT,
+        ReverseT
+    >,
     /**
      * 제너릭 역 추론
      *
@@ -20,7 +33,7 @@ export interface IContainer<
      */
     PElem = T
 > extends IBidirectionalContainer<
-            IContainer.Iterator<T, IteratorT, ReverseT>,
+            IContainer.Iterator<T, SourceT, IteratorT, ReverseT>,
             ReverseT
         >,
         Iterable<T>,
@@ -45,15 +58,24 @@ export interface IContainer<
  * IContainer가 가지는 내부 속성
  */
 export namespace IContainer {
+    /**
+     * SourceT는 파생 타입을 의미한다.
+     * 이로부터 확장된 타입이 자기 자신 (this)를 가리키도록 하기 위해 만들어진 타입이다.
+     * 예를 들어 List class가 IContainer를 통해 구현되었다고 할 때 SourceT는 List 클래스를 가리킨다.
+     */
     export interface Iterator<
-        T,
-        IteratorT extends Iterator<T, IteratorT, ReverseT>,
-        ReverseT extends ReverseIterator<T, IteratorT, ReverseT>
+        T extends Elem,
+        SourceT extends IContainer<T, SourceT, IteratorT, ReverseT, Elem>,
+        IteratorT extends Iterator<T, SourceT, IteratorT, ReverseT, Elem>,
+        ReverseT extends ReverseIterator<T, SourceT, IteratorT, ReverseT, Elem>,
+        Elem = T
     > extends Readonly<IReversableIterator<T, IteratorT, ReverseT>> {}
 
     export interface ReverseIterator<
-        T,
-        IteratorT extends Iterator<T, IteratorT, ReverseT>,
-        ReverseT extends ReverseIterator<T, IteratorT, ReverseT>
+        T extends Elem,
+        SourceT extends IContainer<T, SourceT, IteratorT, ReverseT, Elem>,
+        IteratorT extends Iterator<T, SourceT, IteratorT, ReverseT, Elem>,
+        ReverseT extends ReverseIterator<T, SourceT, IteratorT, ReverseT, Elem>,
+        Elem = T
     > extends Readonly<IReverseIterator<T, IteratorT, ReverseT>> {}
 }
